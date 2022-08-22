65f1366f83d74084e5b61b98d0b1b1e36f559520
yaccovs <yaccovs@gmail.com>
2022-08-22 21:00:15 +0300

    before add routers

---
+++ src/components/Routing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import App from '../App'
+import Home from '../views/Home'
+
+const Routing = () => {
+  return (
+    <>
+    {/* <Home /> */}
+    <App />
+    </>
+  )
+}
+
+export default Routing
