ff13bb770846b9f22df3a823d7e5abdd74576a2c
yaccovs <yaccovs@gmail.com>
2022-08-22 21:32:26 +0300

    Revert "before add routers"
    This reverts commit 65f1366f83d74084e5b61b98d0b1b1e36f559520.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Routing from './components/Routing'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Routing />
+    <App />
   </React.StrictMode>
 );
 
